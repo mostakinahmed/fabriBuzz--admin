cf1e8859f6ea3381d534133acc8a7568fb74f319
mostakinahmed <mostakin4556@gmail.com>
2025-09-07 01:19:42 +0600

    add product

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
   sidebar.classList.toggle("-translate-x-full");
 }
+
+const addBtn = document.getElementById("addBtn");
+
+addBtn.addEventListener("click", async function (e) {
+  e.preventDefault();
+  const form = e.target.form;
+  const data = {
+    name: form.name.value,
+    price: form.price.value,
+    category: form.category.value,
+    description: form.description.value,
+    images: form.images.value,
+  };
+
+  const response = await fetch("http://localhost:3000/api/product", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  const result = await response.text();
+  //reset form and hide it
+  form.reset();
+  form.classList.add("hidden");
+});
